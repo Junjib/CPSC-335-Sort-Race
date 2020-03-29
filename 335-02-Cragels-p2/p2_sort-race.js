594f0a307e27e968212df21b8e65e6e0c836c9ed
Armon16 <42981215+Armon16@users.noreply.github.com>
2020-03-29 16:24:23 -0700

    Add files via upload

--- 335-02-Cragels-p2/p2_sort-race.js
+++ 335-02-Cragels-p2/p2_sort-race.js
@@ -23,21 +23,85 @@ function draw()
 }
 
 function Golds() {
-
+    int sort;
+    sort = 0;
+    //the array we want to sort
     char array[];
-
-    if (array[0] < array[1])
-        x = array[0]
-    y = array[1]
-    array[0] = y
-    array[1] = x
-    so on for the entire array for evens and odds
-
-for (int i = 0; i < 16(length of array); i++
-    if (array[i] > array[i + 1])
-        int count = 0;
-    count++;
-    if (count == 16)
-        done = 1;
-    return 1;
+    while (sort == 0) {
+        //even pairing
+        if (array[0] < array[1]) { 
+            x = array[0]
+            y = array[1]
+            array[0] = y
+            array[1] = x
+        }
+        if (array[2] < array[3]) {
+            x = array[2]
+            y = array[3]
+            array[2] = y
+            array[3] = x
+        }
+        if (array[4] < array[5]) {
+            x = array[4]
+            y = array[5]
+            array[4] = y
+            array[5] = x
+        }
+        if (array[6] < array[7]) {
+            x = array[6]
+            y = array[7]
+            array[6] = y
+            array[7] = x
+        }
+        if (array[8] < array[9]) {
+            x = array[8]
+            y = array[9]
+            array[8] = y
+            array[9] = x
+        }
+        if (array[10] < array[11]) {
+            x = array[10]
+            y = array[11]
+            array[10] = y
+            array[11] = x
+        }
+        //odd pairing
+        if (array[1] < array[2]) {
+            x = array[1]
+            y = array[2]
+            array[1] = y
+            array[2] = x
+        }
+        if (array[3] < array[4]) {
+            x = array[3]
+            y = array[4]
+            array[3] = y
+            array[4] = x
+        }
+        if (array[5] < array[6]) {
+            x = array[5]
+            y = array[6]
+            array[5] = y
+            array[6] = x
+        }
+        if (array[7] < array[8]) {
+            x = array[7]
+            y = array[8]
+            array[7] = y
+            array[8] = x
+        }
+        if (array[9] < array[10]) {
+            x = array[9]
+            y = array[10]
+            array[9] = y
+            array[10] = x
+        }
+        for (int i = 0; i < 12; i+=2)
+        if (array[i] > array[i + 1])
+            int count = 0;
+            count++;
+        if (count == 12)
+            sort = 1;
+        return 1;
+    }
 }
